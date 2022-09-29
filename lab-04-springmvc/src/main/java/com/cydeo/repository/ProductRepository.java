d1d022ffd3560aeb4f66d72bc384b89cfdddd982
Evgenia0707 <4030071@gmail.com>
2022-09-28 22:17:29 -0400

    Spring practice

---
+++ lab-04-springmvc/src/main/java/com/cydeo/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+//package com.cydeo.repository;
+//
+//import com.cydeo.model.Product;
+//
+//import java.util.UUID;
+//import java.util.List;
+//
+//public interface ProductRepository {
+//
+//    boolean save(Product product);
+//    Product findProductById(UUID productId);
+//    List<Product> findAll();
+//}
