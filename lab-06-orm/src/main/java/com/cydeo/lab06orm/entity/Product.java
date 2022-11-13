e6ca0d4380e9be5aa4df25cfe1d8236b7e25b887
Evgenia0707 <4030071@gmail.com>
2022-11-13 11:47:33 -0500

    ORM Lab

---
+++ lab-06-orm/src/main/java/com/cydeo/lab06orm/entity/Product.java
@@ -0,0 +1,28 @@
+package com.cydeo.lab06orm.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product extends BaseEntity {
+
+    private String name;
+    private BigDecimal price;
+    private Integer quantity;
+    private Integer remaining_quantity;
+
+    @ManyToMany
+    @JoinTable(name = "product_category_rel",
+            joinColumns = @JoinColumn(name = "p_id"),
+            inverseJoinColumns = @JoinColumn(name = "c_id"))
+    private List<Category> categoryList;
+}
