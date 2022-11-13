c2a9c3e170cf179839ef935eefb8430da796f813
Evgenia0707 <4030071@gmail.com>
2022-11-13 11:35:39 -0500

    ORM Lab

---
+++ lab-06-orm/src/main/java/com/cydeo/lab06orm/entity/Cart.java
@@ -0,0 +1,25 @@
+package com.cydeo.lab06orm.entity;
+
+import com.cydeo.lab06orm.enums.CartState;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Cart extends BaseEntity {
+
+    @Enumerated(EnumType.STRING)
+    private CartState cartState;
+
+    @ManyToOne
+    private Customer customer;
+
+    @ManyToOne
+    private Discount discount;
+}
