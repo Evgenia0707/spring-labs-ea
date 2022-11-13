0610f4416caf43ce45c4f672ace3d42a341754fd
Evgenia0707 <4030071@gmail.com>
2022-11-13 11:22:33 -0500

    ORM Lab

---
+++ lab-06-orm/src/main/java/com/cydeo/lab06orm/entity/Address.java
@@ -0,0 +1,26 @@
+package com.cydeo.lab06orm.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String name;
+    private String street;
+    private String zipCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Customer customer;
+
+}
